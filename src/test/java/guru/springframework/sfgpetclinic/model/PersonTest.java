a7a61547f73dc13be029cf1329675ef417e6e77f
eduard.streltsov <streltsov.eduard@gmail.com>
2018-12-06 16:15:48 +0200

    S6L67

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,5 +31,15 @@ class PersonTest {
 				() -> assertEquals(person.getFirstName(), "Joe", "first name failed"),
 				() -> assertEquals(person.getLastName(), "Buck", "last name failed"));
 		
+	}
+	
+	@RepeatedTest(value = 10, name = "{currentRepetition} from {totalRepetitions}")
+	void repeatedTest() {
+		System.out.println(" ");
+	}
+	
+	@RepeatedTest(5)
+	void repeatedTestWithId(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+		System.out.println(testInfo.getDisplayName() + " : " + repetitionInfo.getCurrentRepetition());
 	}
 }
